83c2800a264e0304e862d277cf0fa37047515c49
Opeoluwa Aboaba <tomibady@gmail.com>
2021-12-13 23:14:16 +0100

    YouTube API
    I added the YouTube API to Student's Coursework and Video Activities.

--- app/src/main/java/com/iodaniel/mobileclass/student_package/videos/StudentCourseWork.kt
+++ app/src/main/java/com/iodaniel/mobileclass/student_package/videos/StudentCourseWork.kt
@@ -2,11 +2,53 @@ package com.iodaniel.mobileclass.student_package.videos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.iodaniel.mobileclass.R
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
-class StudentCourseWork : AppCompatActivity() {
+class StudentCourseWork : YouTubeBaseActivity() {
+
+    // Change the AppCompactActivity to YouTubeBaseActivity()
+
+    // Add the api key that you had
+    // copied from google API
+    // This is a dummy api key
+    val api_key =  "AIzaSyBbSFcZpBg3OJnwacq8YKa13WJNoC-a1Lk"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_student_course_work)
+        setContentView(R.layout.activity_main)
+
+        // Get reference to the view of Video player
+        val ytPlayer = findViewById<YouTubePlayerView>(R.id.ytPlayer)
+
+        ytPlayer.initialize(api_key, object : YouTubePlayer.OnInitializedListener{
+            // Implement two methods by clicking on red error bulb
+            // inside onInitializationSuccess method
+            // add the video link or the
+            // playlist link that you want to play
+            // In here we also handle the play and pause functionality
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                player?.loadVideo("HzeK7g8cD0Y")
+                player?.play()
+            }
+
+            // Inside onInitializationFailure
+            // implement the failure functionality
+            // Here we will show toast
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Toast.makeText(this , "Video player Failed" , Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
