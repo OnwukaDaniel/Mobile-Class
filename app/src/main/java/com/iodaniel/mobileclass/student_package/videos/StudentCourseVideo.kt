9bc4549d641b4c6f90bc5e71775a8da0208cd38a
Opeoluwa Aboaba <tomibady@gmail.com>
2021-12-13 14:50:44 +0100

    Student's Vides & Coursework
    I've created the activities for Student's video list and courseworks. Its basically UIs, I will be adding the APIs next.

---
+++ app/src/main/java/com/iodaniel/mobileclass/student_package/videos/StudentCourseVideo.kt
@@ -0,0 +1,12 @@
+package com.iodaniel.mobileclass.student_package.videos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iodaniel.mobileclass.R
+
+class StudentCourseVideo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_course_video)
+    }
+}
